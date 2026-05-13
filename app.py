da57e6d0c9ca4efc7afe8d2ed09ae1e9da2f30de
Danyel-xp <danyellrodrigues023@gmail.com>
2026-05-13 13:51:20 -0300

    feat: adicionar cadastro

--- app.py
+++ app.py
@@ -2,11 +2,58 @@ from db import conectar, criar_tabela
 import datetime
 
 criar_tabela()
-# def cadastrar():
+
+def cadastrar():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    try:
+
+        nome = str(input('Nome: '))
+        categoria = str(input('Categoria: '))
+        quantidade = int(input('Quantidade: '))
+        data = str(input('Data YYY-MM-DD: '))
+
+        cursor.execute('''
+            INSERT INTO produtos(nome,categoria,quantidade,data)
+            VALUES(?,?,?,?)
+        ''',(nome,categoria,quantidade,data))
+
+        cursor.execute('''
+            SELECT * FROM produtos
+             WHERE nome LIKE ?
+        ''',(f'%{nome}%',))
+
+        produto = cursor.fetchone()
+
+        print(f'{nome} Cadatrado com sucesso, veja:')
+
+        largura = 30
+
+        print(f'╔{"═" * largura}╗')
+        print(f'║{"PRODUTO" :^ {largura}}║')
+        print(f'╠{"═" * largura}╣')
+        print(f'║ {"Id: " + str(produto[0]) :< { largura - 1 }}║')
+        print(f'║ {"Nome: " + produto[1][:18 ] :< { largura - 1 }}║')
+        print(f'║ {"Categoria: " + produto[2][:20] :< { largura -1 }}║')
+        print(f'║ {"Quantidade: " + str(produto[3])[:18] :< { largura - 1 }}║')
+        print(f'║ {"Data: " + produto[4] :< { largura -1 }}║')
+        print(f'╚{"═" * largura}╝')
+
+        conn.commit()
+        
+    except ValueError:
+        print('Quantidade deve conter apenas números inteiros, por favor revise o erro e tente novamente!')
+    
+    finally:
+        conn.close()
+
 
 # def atualizar():
 
 # def consultar():
+
+# def alerta():
 
 # Simbolos  ╔ ╗ ╚ ╝ ═ ║ ╠ ╣ →
 
@@ -26,8 +73,7 @@ while True:
         opcao = int(input('Escolha: '))
 
         if opcao == 1:
-            print('')
-            # cadastrar()
+            cadastrar()
 
         elif opcao == 2:
 
