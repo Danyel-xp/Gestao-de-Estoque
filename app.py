bce032f95d9516ce0664b35e226b0917bbbdca29
Danyel-xp <danyellrodrigues023@gmail.com>
2026-05-16 13:16:44 -0300

    feat: implementar adição de unidades ao estoque

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 from db import conectar, criar_tabela
-import datetime
+from datetime import datetime
 
 criar_tabela()
 
 def cadastrar():
-    conn = conectar()
-    cursor = conn.cursor()
 
     try:
+        conn = conectar()
+        cursor = conn.cursor()
 
-        nome = str(input('Nome: '))
-        categoria = str(input('Categoria: '))
+        nome = str(input('Nome: ')).strip().lower()
+        categoria = str(input('Categoria: ')).strip().lower()
         quantidade = int(input('Quantidade: '))
-        data = str(input('Data YYY-MM-DD: '))
+        data = str(datetime.today().strftime('%Y/%m/%d às %Hh:%Mm:%Ss'))
 
         cursor.execute('''
             INSERT INTO produtos(nome,categoria,quantidade,data)
@@ -28,28 +28,87 @@ def cadastrar():
 
         print(f'{nome} Cadatrado com sucesso, veja:')
 
-        largura = 30
+        largura = 45
 
         print(f'╔{"═" * largura}╗')
-        print(f'║{"PRODUTO" :^ {largura}}║')
+        print(f'║{"PRODUTO" :^{largura}}║')
         print(f'╠{"═" * largura}╣')
-        print(f'║ {"Id: " + str(produto[0]) :< { largura - 1 }}║')
-        print(f'║ {"Nome: " + produto[1][:18 ] :< { largura - 1 }}║')
-        print(f'║ {"Categoria: " + produto[2][:20] :< { largura -1 }}║')
-        print(f'║ {"Quantidade: " + str(produto[3])[:18] :< { largura - 1 }}║')
-        print(f'║ {"Data: " + produto[4] :< { largura -1 }}║')
+        print(f'║ {"Id: " + str(produto[0]) :<{ largura - 1 }}║')
+        print(f'║ {"Nome: " + produto[1][:18 ] :<{ largura - 1 }}║')
+        print(f'║ {"Categoria: " + produto[2][:20] :<{ largura -1 }}║')
+        print(f'║ {"Quantidade: " + str(produto[3])[:18] :<{ largura - 1 }}║')
+        print(f'║ {"Data: " + produto[4] :<{ largura -1 }}║')
         print(f'╚{"═" * largura}╝')
 
         conn.commit()
         
-    except ValueError:
+    except ValueError as e:
         print('Quantidade deve conter apenas números inteiros, por favor revise o erro e tente novamente!')
+        print(e)
     
     finally:
         conn.close()
 
 
-# def atualizar():
+def atualizar():
+
+    try:
+        conn = conectar()
+        cursor = conn.cursor()
+
+        op = int(input('Escolha: '))
+        
+        if op == 1:
+            nome_produto = str(input('Digite o nome do produto: ')).strip().lower()
+
+            cursor.execute(
+                "SELECT * FROM produtos WHERE nome LIKE ?",(f'%{nome_produto}%',)
+            )
+
+            mostra_produto = cursor.fetchone()
+
+            print(f'Nome: {mostra_produto[1]}')
+            print(f'Categoria: {mostra_produto[2]}')
+            print(f'Quantidade atual: {mostra_produto[3]}')
+            print(f'Ultima data de atualização: {mostra_produto[4]}')
+
+            quantidade_adicionada = int(input('Quantas unidades você quer adicionar: '))
+
+            escolha = str(input(f'Tem certeza que deseja adicionar novas {quantidade_adicionada} unidades [S/N]:  '))
+
+            if escolha == 'S':
+
+                cursor.execute(
+                    'SELECT * FROM produtos WHERE nome LIKE ?',(f'%{nome_produto}%',)
+                )
+
+                produto = cursor.fetchone()
+
+                cursor.execute(
+                    'UPDATE produtos SET quantidade = ? WHERE nome LIKE ?',(produto[3] + quantidade_adicionada, f'%{nome_produto}%')
+                )
+
+                print(f'{nome_produto} atualizado com sucesso!')
+                print(f'Qauntidade anterior: {produto[3]} ')
+                print(f'Quantidade adicionada: {quantidade_adicionada}')
+                print(f'Quantidade atual: {produto[3] + quantidade_adicionada}')
+
+                conn.commit()
+
+            elif escolha == 'N':
+                print(f'A atualização de {mostra_produto[1]} foi cancelada!')
+
+            else:
+                print('Entrada inválida, tente novamente!')
+
+    except ValueError as e :
+        print(e)
+    
+    finally:
+        conn.close()
+
+
+    
 
 # def consultar():
 
@@ -84,7 +143,7 @@ while True:
             print('║ 2 → Retirar unidades        ║')
             print('╚═════════════════════════════╝')
 
-            # atualizar()
+            atualizar()
 
         elif opcao == 3:
             
--- db.py
+++ db.py
@@ -20,3 +20,4 @@ def criar_tabela():
 
     conn.commit()
     conn.close()
+
